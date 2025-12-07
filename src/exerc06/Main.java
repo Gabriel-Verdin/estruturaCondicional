730350e4e1f9e4d0100a5233036da03dff03479a
Gabriel Verdin <gabrielg.verdin@gmail.com>
2025-12-07 19:52:53 -0300

    Termino dos exercicios Java

--- src/exerc06/Main.java
+++ src/exerc06/Main.java
@@ -1,4 +1,7 @@
-// ========================= NÃO TERMINADO ========================= 
+
+// Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos
+// seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em
+// nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
 
 package exerc06;
 
@@ -19,6 +22,18 @@ public class Main {
 		
 		if(num >= 0 && num <= 25) {
 			System.out.println("O intervalo é: [0, 25]");
+		}
+		else if(num > 25 && num <= 50) {
+			System.out.println("O intervalo é: [25, 50]");
+		}
+		else if(num > 50 && num <= 75) {
+			System.out.println("O intervalo é: [50, 75]");
+		}
+		else if(num > 75 && num <= 100) {
+			System.out.println("O intervalo é: [75, 100]");
+		}
+		else {
+			System.out.println("Fora de Intervalo!");
 		}
 		
 		sc.close();
